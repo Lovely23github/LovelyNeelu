d3122dd2f2eb32fcc5adc3c134526510f004ffe9
Lovely <lovely02012000@gmail.com>
2023-11-17 16:09:37 +0530

    commit to branch 1

--- src/DemoNeelu.java
+++ src/DemoNeelu.java
@@ -1,5 +1,13 @@
 public class DemoNeelu {
     public static void main(String[] args ){
         System.out.println("Hello People");
+        int number=7;
+        if(number%2==0){
+            System.out.println("Even Number");
+        }
+        else {
+            System.out.println("Odd NUmber");
+        }
     }
+
 }
